050e7f6515a0d7afd92a1fe21221329b50b31166
Lizzie <lchan217@gmail.com>
2019-08-06 09:49:17 -0500

    add initial draft for contact me

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,11 @@ import React from 'react';
 const Contact = () => {
   return (
     <div className="contact">
+      <h1> Contact Me </h1>
+      <div>Email: Lchan217@gmail.com</div><br></br>
+      <div><a href="https://www.linkedin.com/in/elizabethchan1234/">LinkedIn</a></div>
+      <div><a href="https://github.com/lchan217">GitHub</a></div>
+
     </div>
   );
 };
