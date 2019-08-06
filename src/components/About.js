53f424fd05abccf45dd50015337a2f255cbdf174
Lizzie <lchan217@gmail.com>
2019-08-06 09:42:39 -0500

    add initial draft of about me

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,25 @@ import React from 'react';
 const About = () => {
   return (
     <div className="aboutMe">
+      <h2>Who Am I?</h2>
+        <p>I am a University of Dayton grad, full stack web developer, and Certified Public Accountant with five years of
+        experience in financial accounting.
+        I have financial competence closing time-sensitive financial statements, and I have expertise using systems such as SAP,
+        Blackline, and Hyperion Financial Management. </p>
+
+        <p>In November 2017, I quit my job to travel Southeast Asia with my friend and solo travel in Europe. When I came back to
+        the states, a friend who had done a coding bootcamp thought it would be a great fit for me. I did a few sample courses
+        and really liked the way of thinking, plus coding was one of the top jobs for remote work! One day, I hope to travel
+        while working. </p>
+
+        <p>Attention to detail and ability to think logically have made learning Rails,
+        JavaScript, and React a strength of mine. I look forward to expanding my knowledge of languages, contributing my
+        persistent work ethic, and changing my career to something I am passionate about.</p>
+
+        <p>Check out the Bonus tab for some of my favorite things. I am always happy to give
+        advice and hear recommendations!</p>
+
+        <b>INSERT photos from travel </b>
     </div>
   );
 };
