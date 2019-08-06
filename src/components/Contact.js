4d68a8f58e01ea4a36063dd8dfed38aea24e00b1
Lizzie <lchan217@gmail.com>
2019-08-06 17:12:54 -0500

    add linkedin and github icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,8 +5,8 @@ const Contact = () => {
     <div className="contact">
       <h1> Contact Me </h1>
       <div>Email: Lchan217@gmail.com</div><br></br>
-      <div><a href="https://www.linkedin.com/in/elizabethchan1234/">LinkedIn</a></div>
-      <div><a href="https://github.com/lchan217">GitHub</a></div>
+      <div><i class="large linkedin icon"></i><a href="https://www.linkedin.com/in/elizabethchan1234/">LinkedIn</a></div>
+      <div><i class="large github icon"></i><a href="https://github.com/lchan217">GitHub</a></div>
 
     </div>
   );
