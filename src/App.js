9740f287386ed08559b8514b84a69ed03920d696
Lizzie <lchan217@gmail.com>
2019-08-06 09:23:06 -0500

    add paths to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+
+import Home from './components/Home'
+import About from './components/About'
+import Projects from './components/Projects'
+import Resume from './components/Resume'
+import Contact from './components/Contact'
 
 function App() {
   return (
-    <div className="App">
-      test
-    </div>
-  );
+    <Router>
+      <div>
+        <NavBar />
+        <Route exact path='/'  component={Home} />
+        <Route exact path='/about'  component={About} />
+        <Route exact path='/projects'  component={Projects} />
+        <Route exact path='/resume'  component={Resume} />
+        <Route exact path='/contact'  component={Contact} />
+      </div>
+    </Router>
+  )
 }
 
 export default App;
