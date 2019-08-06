3aeaf04e533063839c695e305e6c618da9e22952
Lizzie <lchan217@gmail.com>
2019-08-06 15:23:00 -0500

    add draft of interests

--- src/components/OtherInterests.js
+++ src/components/OtherInterests.js
@@ -3,9 +3,32 @@ import React from 'react';
 const OtherInterests = () => {
   return (
     <div className="otherInterests">
-      <div>Map of places you have travelled </div>
-      <div>Top Reading List </div>
-      <div>Top Podcast List </div>
+      <h1>Favorite Places</h1>
+        Get map from Brittany
+        <ol>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ol>
+
+      <h1>Favorite Books</h1>
+        <ol>
+          <li>Shutter Island by Dennis Lehane</li>
+          <li>Hannibal Lecter Series by Thomas Harris</li>
+          <li>Before I Go To Sleep by S.J Watson</li>
+          <li>Pet Sematary by Stephen King</li>
+          <li>Before We Were Yours by Lisa Wingate</li>
+        </ol>
+      <h1>Favorite Podcasts </h1>
+        <ol>
+          <li>Radiolab</li>
+          <li>Reply All</li>
+          <li>How I Built This</li>
+          <li>I, Survivor</li>
+          <li>Piano Puzzler</li>
+        </ol>
     </div>
   );
 };
