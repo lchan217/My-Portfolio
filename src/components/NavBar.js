b89b699325899921856d1e557de50bf7265e7389
Lizzie <lchan217@gmail.com>
2019-08-06 16:19:28 -0500

    change format of navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="navBar">
-      <div><Link to='/'>Home</Link></div>
-      <div><Link to='/about'>About Me</Link></div>
-      <div><Link to='/projects'>Projects</Link></div>
-      <div><Link to='/experience'>Experience</Link></div>
-      <div><Link to='/interests'>Other Interests</Link></div>
-      <div><Link to='/contact'>Contact</Link></div>
+      <div class="ui six item menu">
+        <div className="item"><Link to='/'>Home</Link></div>
+        <div className="item"><Link to='/about'>About Me</Link></div>
+        <div className="item"><Link to='/projects'>Projects</Link></div>
+        <div className="item"><Link to='/experience'>Experience</Link></div>
+        <div className="item"><Link to='/interests'>Other Interests</Link></div>
+        <div className="item"><Link to='/contact'>Contact</Link></div>
+      </div>
     </div>
   );
 };
