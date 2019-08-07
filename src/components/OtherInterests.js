dd1fa53e579d13f58058bd60bf532808dbc724e1
Lizzie <lchan217@gmail.com>
2019-08-07 09:41:40 -0500

    add ui items to fav places

--- src/components/OtherInterests.js
+++ src/components/OtherInterests.js
@@ -4,6 +4,75 @@ const OtherInterests = () => {
   return (
     <div className="otherInterests">
 
+        <h1>Favorite Places</h1>
+        <div class="ui items">
+          <div class="item">
+              <div class="ui medium image">
+                <img src="https://i.ibb.co/dKKxXDB/GOPR2489-1522143485766-high.jpg" alt="GOPR2489-1522143485766-high" border="0"></img>
+              </div>
+              <div class="content">
+                <div class="header">Siquijor, Philippines</div>
+                <div class="description">
+                  <p>Small island where you could snorkel right off any beach</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="item">
+                <div class="ui medium image">
+                  <img src="https://i.ibb.co/3cc27mJ/20180426-083944.jpg" alt="20180426-083944" border="0"></img>
+                </div>
+                <div class="content">
+                  <div class="header">Chengdu, China</div>
+                  <div class="description">
+                    <p>Home of a panda breeding center, which was the inspiration for my last project</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="item">
+                  <div class="ui medium image">
+                    <img src="https://i.ibb.co/qYzLj4w/20180418-112934.jpg" alt="20180418-112934" border="0"></img>
+                  </div>
+                  <div class="content">
+                    <div class="header">Hong Kong</div>
+                    <div class="description">
+                      <p>The perfect combination of city and greenery</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="item">
+                  <div class="ui medium image">
+                    <img src="https://i.ibb.co/PrGBMYy/20180306-043935.jpg" alt="20180306-043935" border="0"></img>
+                  </div>
+                  <div class="content">
+                    <div class="header">Mount Ijen, Indonesia</div>
+                    <div class="description">
+                      <p>Difficult hike to see the inside of a volcano</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="item">
+                  <div class="ui medium image">
+                    <img src="https://i.ibb.co/bd0fw0p/20180313-164458.jpg" alt="20180313-164458" border="0"></img>
+                  </div>
+                  <div class="content">
+                    <div class="header">Temanggung, Indonesia</div>
+                    <div class="description">
+                      <p>We had the opportunity to live with a family and teach English to local children</p>
+                  </div>
+                </div>
+              </div>
+
+        </div>
+
+          <p>Below is a photo of the backpacking route my friend and I covered in about 6 months. It was hard to pick just 5 places! </p>
+        <img src="https://i.ibb.co/1LMY3B2/db488e75-bec2-4d12-8bb4-1ebaef1ebcec.jpg" alt="db488e75-bec2-4d12-8bb4-1ebaef1ebcec" border="0"></img>
+
+      <div class="ui divider"></div>
+
       <h1>Favorite Books</h1>
         <div class="ui items">
           <div class="item">
@@ -152,19 +221,8 @@ const OtherInterests = () => {
                     </div>
                   </div>
 
-    <div class="ui divider"></div>
 
-      <h1>Favorite Places</h1>
-      <p>Below is a photo of the backpacking route my friend and I covered in about 6 months. We hopped all over the place,
-      but here are my 5 favorite and why. </p>
-        <ol>
-          <li>Siquijor, Philippines - small island where you could snorkel right off any beach</li>
-          <li>Chengdu, China - home of a panda breeding center, which was the inspiration for my last project</li>
-          <li>Hong Kong - the perfect combination of city and greenery</li>
-          <li>Mount Ijen, Indonesia - difficult hike to see the inside of a volcano</li>
-          <li>Temanggung, Indonesia - we had the opportunity to live with a family and teach English to local children</li>
-        </ol>
-      <img src="https://i.ibb.co/1LMY3B2/db488e75-bec2-4d12-8bb4-1ebaef1ebcec.jpg" alt="db488e75-bec2-4d12-8bb4-1ebaef1ebcec" border="0"></img>
+
     </div>
   );
 };
