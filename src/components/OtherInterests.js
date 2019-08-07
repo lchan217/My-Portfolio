6ead2a9df81e09ff776d28b2542bae64e0fcf9c3
Lizzie <lchan217@gmail.com>
2019-08-07 09:13:24 -0500

    add ui items to fav podcasts

--- src/components/OtherInterests.js
+++ src/components/OtherInterests.js
@@ -7,61 +7,150 @@ const OtherInterests = () => {
       <h1>Favorite Books</h1>
         <div class="ui items">
           <div class="item">
-            <div class="ui tiny image">
-              <img src="https://i.harperapps.com/covers/9780061898815/x510.jpg"></img>
+              <div class="ui small image">
+                <img src="https://i.harperapps.com/covers/9780061898815/x510.jpg"></img>
+              </div>
+              <div class="content">
+                <div class="header">Shutter Island</div>
+                <div class="meta">
+                  <span class="author">Dennis Lehane</span> |
+                  <span class="genre"> Thriller</span>
+                </div>
+                <div class="description">
+                  <p>add a description</p>
+                </div>
+              </div>
             </div>
-            <div class="middle aligned content">
-              <div class="header">Shutter Island by Dennis Lehane</div>
-            </div>
-          </div>
 
-          <div class="item">
-            <div class="ui tiny image">
-              <img src="https://i.ebayimg.com/00/s/MTYwMFgxMTA5/z/rocAAOSwdwVcG7xY/$_57.JPG"></img>
-            </div>
-            <div class="middle aligned content">
-              <div class="header">Hannibal Lecter Series by Thomas Harris</div>
-            </div>
-          </div>
+            <div class="item">
+                <div class="ui small image">
+                  <img src="https://i.ebayimg.com/00/s/MTYwMFgxMTA5/z/rocAAOSwdwVcG7xY/$_57.JPG"></img>
+                </div>
+                <div class="content">
+                  <div class="header">Hannibel Lecter Series</div>
+                  <div class="meta">
+                    <span class="author">Thomas Harris</span> |
+                    <span class="genre"> Thriller</span>
+                  </div>
+                  <div class="description">
+                    <p>add a description</p>
+                  </div>
+                </div>
+              </div>
 
-          <div class="item">
-            <div class="ui tiny image">
-              <img src="https://images-na.ssl-images-amazon.com/images/I/51fYg6NRV2L._SX329_BO1,204,203,200_.jpg"></img>
-            </div>
-            <div class="middle aligned content">
-              <div class="header">Before I Go To Sleep by S.J Watson</div>
-            </div>
-          </div>
+              <div class="item">
+                  <div class="ui small image">
+                    <img src="https://images-na.ssl-images-amazon.com/images/I/51fYg6NRV2L._SX329_BO1,204,203,200_.jpg"></img>
+                  </div>
+                  <div class="content">
+                    <div class="header">Before I Go To Sleep</div>
+                    <div class="meta">
+                      <span class="author">S.J. Watson</span> |
+                      <span class="genre"> Thriller</span>
+                    </div>
+                    <div class="description">
+                      <p>add a description</p>
+                    </div>
+                  </div>
+                </div>
 
-          <div class="item">
-            <div class="ui tiny image">
-              <img src="https://images-na.ssl-images-amazon.com/images/I/81efI4Y5MFL.jpg"></img>
-            </div>
-            <div class="middle aligned content">
-              <div class="header">Pet Sematary by Stephen King</div>
-            </div>
-          </div>
+                <div class="item">
+                    <div class="ui small image">
+                      <img src="https://images-na.ssl-images-amazon.com/images/I/81efI4Y5MFL.jpg"></img>
+                    </div>
+                    <div class="content">
+                      <div class="header">Pet Sematary</div>
+                      <div class="meta">
+                        <span class="author">Stephen King</span> |
+                        <span class="genre"> Thriller</span>
+                      </div>
+                      <div class="description">
+                        <p>add a description</p>
+                      </div>
+                    </div>
+                  </div>
 
-          <div class="item">
-            <div class="ui tiny image">
-              <img src="https://images-na.ssl-images-amazon.com/images/I/91vrKey6uML.jpg"></img>
-            </div>
-            <div class="middle aligned content">
-              <div class="header">Before We Were Yours by Lisa Wingate</div>
-            </div>
-          </div>
+                  <div class="item">
+                      <div class="ui small image">
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/91vrKey6uML.jpg"></img>
+                      </div>
+                      <div class="content">
+                        <div class="header">Before We Were Yours</div>
+                        <div class="meta">
+                          <span class="author">Lisa Wingate</span> |
+                          <span class="genre"> Historical Fiction</span>
+                        </div>
+                        <div class="description">
+                          <p>add a description</p>
+                        </div>
+                      </div>
+                    </div>
         </div>
 
   <div class="ui divider"></div>
 
       <h1>Favorite Podcasts </h1>
-        <ol>
-          <li>Radiolab</li>
-          <li>Reply All</li>
-          <li>How I Built This</li>
-          <li>I, Survivor</li>
-          <li>Piano Puzzler</li>
-        </ol>
+        <div class="ui items">
+          <div class="item">
+              <div class="ui small image">
+                <img src="https://media.npr.org/images/podcasts/primary/icon_452538884-e2b3a7cfb90f8695d1773e05a81788dbe544e535.png?s=1400"></img>
+              </div>
+              <div class="content">
+                <div class="header">Radiolab</div>
+                <div class="description">
+                  <p>add a description</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="item">
+                <div class="ui small image">
+                  <img src="http://www.xfdrmag.net/wp-content/uploads/2017/05/podcast-of-the-week-reply-all.png"></img>
+                </div>
+                <div class="content">
+                  <div class="header">Reply All</div>
+                  <div class="description">
+                    <p>add a description</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="item">
+                  <div class="ui small image">
+                    <img src="https://factory45.co/wp-content/uploads/2017/07/howibuiltthis-1500x833.jpg"></img>
+                  </div>
+                  <div class="content">
+                    <div class="header">How I Built This</div>
+                    <div class="description">
+                      <p>add a description</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="item">
+                    <div class="ui small image">
+                      <img src="https://media.npr.org/images/podcasts/primary/icon_381443927-4df9626ca846ba53255c29d18ae2c99d820b5d3c.jpg?s=1400"></img>
+                    </div>
+                    <div class="content">
+                      <div class="header">Piano Puzzler</div>
+                      <div class="description">
+                        <p>add a description</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="item">
+                      <div class="ui small image">
+                        <img src="https://wondery.com/wp-content/uploads/2019/05/Survivor-300x300.jpeg"></img>
+                      </div>
+                      <div class="content">
+                        <div class="header">I, Survivor</div>
+                        <div class="description">
+                          <p>add a description</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
 
     <div class="ui divider"></div>
 
