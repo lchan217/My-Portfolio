6f5b47b4adc12b8e45fcc60d3bf652990713a6c8
Lizzie <lchan217@gmail.com>
2019-08-07 08:57:40 -0500

    add ui items to fav books

--- src/components/OtherInterests.js
+++ src/components/OtherInterests.js
@@ -3,24 +3,57 @@ import React from 'react';
 const OtherInterests = () => {
   return (
     <div className="otherInterests">
-      <h1>Favorite Places</h1>
-        Get map from Brittany
-        <ol>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ol>
 
       <h1>Favorite Books</h1>
-        <ol>
-          <li>Shutter Island by Dennis Lehane</li>
-          <li>Hannibal Lecter Series by Thomas Harris</li>
-          <li>Before I Go To Sleep by S.J Watson</li>
-          <li>Pet Sematary by Stephen King</li>
-          <li>Before We Were Yours by Lisa Wingate</li>
-        </ol>
+        <div class="ui items">
+          <div class="item">
+            <div class="ui tiny image">
+              <img src="https://i.harperapps.com/covers/9780061898815/x510.jpg"></img>
+            </div>
+            <div class="middle aligned content">
+              <div class="header">Shutter Island by Dennis Lehane</div>
+            </div>
+          </div>
+
+          <div class="item">
+            <div class="ui tiny image">
+              <img src="https://i.ebayimg.com/00/s/MTYwMFgxMTA5/z/rocAAOSwdwVcG7xY/$_57.JPG"></img>
+            </div>
+            <div class="middle aligned content">
+              <div class="header">Hannibal Lecter Series by Thomas Harris</div>
+            </div>
+          </div>
+
+          <div class="item">
+            <div class="ui tiny image">
+              <img src="https://images-na.ssl-images-amazon.com/images/I/51fYg6NRV2L._SX329_BO1,204,203,200_.jpg"></img>
+            </div>
+            <div class="middle aligned content">
+              <div class="header">Before I Go To Sleep by S.J Watson</div>
+            </div>
+          </div>
+
+          <div class="item">
+            <div class="ui tiny image">
+              <img src="https://images-na.ssl-images-amazon.com/images/I/81efI4Y5MFL.jpg"></img>
+            </div>
+            <div class="middle aligned content">
+              <div class="header">Pet Sematary by Stephen King</div>
+            </div>
+          </div>
+
+          <div class="item">
+            <div class="ui tiny image">
+              <img src="https://images-na.ssl-images-amazon.com/images/I/91vrKey6uML.jpg"></img>
+            </div>
+            <div class="middle aligned content">
+              <div class="header">Before We Were Yours by Lisa Wingate</div>
+            </div>
+          </div>
+        </div>
+
+  <div class="ui divider"></div>
+
       <h1>Favorite Podcasts </h1>
         <ol>
           <li>Radiolab</li>
@@ -29,6 +62,20 @@ const OtherInterests = () => {
           <li>I, Survivor</li>
           <li>Piano Puzzler</li>
         </ol>
+
+    <div class="ui divider"></div>
+
+      <h1>Favorite Places</h1>
+      <p>Below is a photo of the backpacking route my friend and I covered in about 6 months. We hopped all over the place,
+      but here are my 5 favorite and why. </p>
+        <ol>
+          <li>Siquijor, Philippines - small island where you could snorkel right off any beach</li>
+          <li>Chengdu, China - home of a panda breeding center, which was the inspiration for my last project</li>
+          <li>Hong Kong - the perfect combination of city and greenery</li>
+          <li>Mount Ijen, Indonesia - difficult hike to see the inside of a volcano</li>
+          <li>Temanggung, Indonesia - we had the opportunity to live with a family and teach English to local children</li>
+        </ol>
+      <img src="https://i.ibb.co/1LMY3B2/db488e75-bec2-4d12-8bb4-1ebaef1ebcec.jpg" alt="db488e75-bec2-4d12-8bb4-1ebaef1ebcec" border="0"></img>
     </div>
   );
 };
