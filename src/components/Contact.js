e8e6f7a051f360729e28734e329af8b088ce60e9
Lizzie <lchan217@gmail.com>
2019-08-08 11:26:48 -0500

    minor change to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,9 +4,9 @@ const Contact = () => {
   return (
     <div className="contact">
       <h1> Contact Me </h1>
-      <div>Email: Lchan217@gmail.com</div><br></br>
-      <div><i class="large linkedin icon"></i><a href="https://www.linkedin.com/in/elizabethchan1234/">LinkedIn</a></div>
-      <div><i class="large github icon"></i><a href="https://github.com/lchan217">GitHub</a></div>
+      <div><i class="envelope outline icon"></i>Email: Lchan217@gmail.com</div><br></br>
+      <div><i class="large linkedin icon"></i><a href="https://www.linkedin.com/in/elizabethchan1234/" target="_blank">LinkedIn</a></div>
+      <div><i class="large github icon"></i><a href="https://github.com/lchan217" target="_blank">GitHub</a></div>
 
     </div>
   );
