51bad314e9bf617e600d503d33b6eaac19d94b29
Lizzie <lchan217@gmail.com>
2019-10-14 09:07:48 -0700

    wording change, add border

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div className="contactInfo" style={{textAlign:"center"}}>
       <h1> Get In Touch </h1>
-        Feel free to reach out with any questions. I am always happy to help! <br></br><br></br>
+        Feel free to reach out with any questions. I am always happy to connect! <br></br><br></br>
           <a href="mailto:lchan217@gmail.com"><i class="envelope outline icon"></i></a>
           <a href="https://www.linkedin.com/in/elizabethchan1234/" target="_blank"><i class="large linkedin icon"></i></a>
           <a href="https://github.com/lchan217" target="_blank"><i class="large github icon"></i></a>
