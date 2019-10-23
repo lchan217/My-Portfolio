c4372d66d8c3d49a2395c23d8792f095725e32e8
Lizzie <lchan217@gmail.com>
2019-10-23 15:50:39 -0700

    add interests to bio

--- src/components/About.js
+++ src/components/About.js
@@ -13,16 +13,21 @@ const About = () => {
         <p>I am a full stack web developer and Certified Public Accountant with five years of
         experience in financial accounting.
         I have financial competence closing time-sensitive financial statements, and I have expertise using systems such as SAP,
-        Blackline, and Hyperion Financial Management. </p>
+        Blackline, and Hyperion Financial Management. Attention to detail and ability to think logically have made learning Rails,
+        JavaScript, and React a strength of mine. </p>
 
       <p>In November 2017, I took a gap year to travel Southeast Asia with my friend and solo travel in Europe. When I came back to
         the states, a friend who had done a coding bootcamp thought it would be a great fit for me. I did a few sample courses
         and really liked the way of thinking, plus coding was one of the top jobs for remote work! One day, I hope to travel
-        while working. </p>
+        while working or live in a foreign country. </p>
 
-        <p>Attention to detail and ability to think logically have made learning Rails,
-        JavaScript, and React a strength of mine. I look forward to expanding my knowledge of languages, contributing my
-        persistent work ethic, and changing my career to something I am passionate about.</p>
+      <p>In my spare time, you can find me reading a book, listening to a podcast, or trying
+        a new type of cheese. My favorite genres are suspense, true crime, and comedy, although, I am always
+        willing to give any book or podcast a shot.</p>
+
+      <p>
+        I look forward to expanding my knowledge of languages, contributing my
+        persistent work ethic, and changing my career to something I am passionate about!</p>
 
         <p></p>
       <div className="contactInfo" style={{textAlign:"center"}}>
