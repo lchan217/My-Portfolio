61e9571eb38b712af2b70915b9790b73679d9484
Lizzie <lchan217@gmail.com>
2019-11-01 19:12:40 -0700

    add semantic divider, title, and pub date

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Blogs = ({title, content}) => {
+const Blogs = ({title, content, thumbnail, guid, link, pubDate}) => {
   return (
     <div className="blog-feed">
-      {title}
+      <div class="ui segment">
+        <div class="ui hidden divider">
+          <a href={guid} target='_blank'>{title}</a></div>
+          <div>published on {pubDate}</div>
+        </div>
     </div>
   );
 };
