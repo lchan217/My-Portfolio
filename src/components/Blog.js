ec443d4d264df595eede8b0e9c12ac8ed190f118
Lizzie <lchan217@gmail.com>
2019-11-01 18:19:24 -0700

    add blog fetch request

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Blog = () => {
-  return (
-    <div className="blog">
-    <h1>heres my blog</h1>
-    </div>
-  );
-};
+class Blog extends Component {
+  state = {
+    blogPosts: []
+  }
+  componentDidMount() {
+   fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@lchan217')
+     .then(response => response.json())
+     .then(data => {
+       this.setState({
+         blogPosts: data
+       })
+     })
+ }
+
+  render(){
+    return (
+      <div className="blog">
+      <h1>My Blog Feed</h1>
+      </div>
+    );
+  };
+}
 
 export default Blog;
