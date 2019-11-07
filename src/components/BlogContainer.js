2eb701a081edef1af1263c377c2f848907e1c32a
Lizzie <lchan217@gmail.com>
2019-11-07 10:48:55 -0700

    adding loading to blog page

--- src/components/BlogContainer.js
+++ src/components/BlogContainer.js
@@ -3,25 +3,36 @@ import Blogs from './Blogs'
 
 class BlogContainer extends Component {
   state = {
-    blogs: []
+    blogs: [],
+    isLoading: true
   }
   componentDidMount() {
    fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@lchan217')
     .then(response => response.json())
     .then((data) => {
        this.setState({
-         blogs: data.items
+         blogs: data.items,
+         isLoading: false
        })
     })
  }
 
   render(){
 
+    let data;
+    if (this.state.isLoading) {
+      data = "Loading..."
+    } else {
+      data = <div>
+                {this.state.blogs.map((blog, index) => <Blogs key={index} {...blog}/>)}
+            </div>
+    }
+
     return (
       <div className="blog-feed">
         <h1 style={{textAlign:"center"}}>My Blogs</h1>
         <br/>
-        {this.state.blogs.map((blog, index) => <Blogs key={index} {...blog}/>)}
+      {data}
       </div>
     );
   };
