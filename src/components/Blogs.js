61b49743991ca776316d64f95261d3c245ce7868
Lizzie <lchan217@gmail.com>
2019-12-15 14:00:57 -0700

    add opacity

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -3,26 +3,27 @@ import Moment from "react-moment";
 
 const Blogs = ({ title, content, thumbnail, guid, link, pubDate }) => {
   return (
-    <div className="blog-feed">
-      <div class="ui segment">
-        <div class="ui hidden divider">
+    <div className='blog-feed'>
+      <div class='ui segment'>
+        <div class='ui hidden divider'>
           <h3>{title}</h3>
         </div>
         <br />
         <div>
           <i>
-            Published On <Moment format="DD/MM/YYYY">{pubDate}</Moment>
+            Published On <Moment format='DD/MM/YYYY'>{pubDate}</Moment>
           </i>
         </div>
         <p>
           {content.substr(0, 300).replace(/<[^>]*>?/gm, "")}... <br />
           <br />
-          <a href={guid} target="_blank">
+          <a href={guid} target='_blank'>
             {" "}
             Continue Reading >>{" "}
           </a>
         </p>
       </div>
+      <br />
     </div>
   );
 };
