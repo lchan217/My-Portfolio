2afc55d289def1b6e3933ab8637580cf8d4a4c65
Lizzie <lchan217@gmail.com>
2019-12-15 14:23:40 -0700

    fixed nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 // <div className="item"><Link to='/interests'>Other Interests</Link></div>
 
 const NavBar = () => {
   return (
-    <div className="navBar">
-      <div class="ui five item menu">
-        <div className="item"><Link to='/'>About Me</Link></div>
-        <div className="item"><Link to='/projects'>Projects</Link></div>
-        <div className="item"><Link to='/experience'>Other Experience</Link></div>
-        <div className="item"><Link to='/blog'>Blog</Link></div>
-        <div className="item"><Link to='/contact'>Contact</Link></div>
+    <div className='navBar'>
+      <div class='ui fixed five item menu'>
+        <div className='item'>
+          <Link to='/'>About Me</Link>
+        </div>
+        <div className='item'>
+          <Link to='/projects'>Projects</Link>
+        </div>
+        <div className='item'>
+          <Link to='/experience'>Other Experience</Link>
+        </div>
+        <div className='item'>
+          <Link to='/blog'>Blog</Link>
+        </div>
+        <div className='item'>
+          <Link to='/contact'>Contact</Link>
+        </div>
       </div>
     </div>
   );
