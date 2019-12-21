72cb0f1c5375c8f8961a53127c0148d28551b9a3
Lizzie <lchan217@gmail.com>
2019-12-21 16:38:36 -0700

    update about me with new paragraphs

--- src/components/About.js
+++ src/components/About.js
@@ -23,33 +23,37 @@ const About = () => {
       <br />
       <p>
         I am a Full Stack Web Developer and Certified Public Accountant with
-        five years of experience in financial accounting. Attention to detail
-        and ability to think logically in all areas of life have made learning
-        Rails, JavaScript, and React a strength of mine.
+        five years of experience in financial accounting. I recently graduated
+        from Flatiron School’s Full Stack Software Engineering Program and I’m
+        excited to begin working in a country outside of the United States!
+        Attention to detail and ability to think logically in all areas of life
+        have made learning Ruby on Rails, JavaScript, and React a strength of
+        mine.
       </p>
       <p>
         Prior to Flatiron School, I graduated from the University of Dayton with
         degrees in Accounting and Music Performance, then moved back to Chicago
-        to work in corporate accounting. I have financial competence closing
-        time-sensitive financial statements, and I have expertise using systems
-        such as SAP, Blackline, and Hyperion Financial Management. During this
-        time, I worked in a shared service center, appeasing customer needs and
-        proactively giving them data they didn’t know they needed.
+        to work in corporate accounting and pass the Certified Public Accounting
+        Exam. I have financial competence closing time-sensitive financial
+        statements, and I have expertise using systems such as SAP, Blackline,
+        and Hyperion Financial Management. During this time, I worked in a
+        shared service center, appeasing customer needs and proactively
+        providing them data before they realized they needed it.
       </p>
       <p>
-        In November 2017, I took a gap year to travel Southeast Asia with my
-        friend and solo travel in Europe. When I came back to the states, a
-        friend who had done a coding bootcamp thought it would be a great fit
-        for me. I did a few sample lessons and things really clicked for me. My
-        brain is meant for coding in the way my thoughts flow and things all
-        come together logically. I am really happy I made the choice to change
-        careers. Plus coding was one of the top jobs for remote work!
+        In November 2017, I took a gap year to travel Southeast Asia and some of
+        Europe. When I came back to the states, I talked with a friend who had
+        completed a coding bootcamp and he thought it would be a great fit for
+        me. I enjoyed the sample lessons and liked how passing the tests and
+        immediately getting the desired output in the browser naturally clicked
+        for me. My brain is meant for coding because I am able to break down a
+        big process in detail and follow the flow to pinpoint where a bug may
+        lie.
       </p>
       <p>
-        I can’t wait to start working in a foreign country, when I will know
-        what languages to focus on and become an expert in. More importantly,
-        it’s exciting to be learning something new every day in a group and
-        independently.
+        I look forward to expanding my knowledge of languages, contributing my
+        persistent work ethic, and changing my career to something I am
+        passionate about.
       </p>
 
       <br />
