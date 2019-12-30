9662715f457c270952b6e828fd88d37960d3d5cd
Lizzie <lchan217@gmail.com>
2019-12-30 13:46:55 -0700

    update resume link & resume data

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -52,7 +52,7 @@ class Experience extends Component {
         <br />
         <br />
         <a
-          href='https://drive.google.com/file/d/1Ih7PokJhQTVpfmHjc5ZtJVmoYlAPhtKR/view?usp=sharing'
+          href='https://drive.google.com/file/d/1lAC-34PrL79qiQU-1pVWatvm8LiGUp-u/view?usp=sharing'
           target='_blank'
         >
           <i class='download icon'></i>VIEW FULL RESUME
@@ -72,7 +72,7 @@ class Experience extends Component {
           <Grid.Column key={9}>CSS</Grid.Column>
           <Grid.Column key={10}>HTML</Grid.Column>
           <Grid.Column key={11}>Semantic UI</Grid.Column>
-          <Grid.Column key={12}>Bootstrap</Grid.Column>
+          <Grid.Column key={12}>Git</Grid.Column>
         </Grid>
         {/* <div class='ui grid'>
         <div class='five wide column'>Ruby on Rails</div>
@@ -100,38 +100,15 @@ class Experience extends Component {
         </i>
         <ul>
           <li>
-            Led team of three when making and implementing accrual process
-            decisions
-          </li>
-          <li>
-            Collaborated with five finance plant managers to meet crucial
-            month-end close deadlines
-          </li>
-          <li>
-            {" "}
             Analyzed the general ledger and identified variances to plant
-            managers ahead of schedule{" "}
+            managers ahead of schedule
           </li>
           <li>
             Utilizing pivot tables, vlookups and conditional formatting in
-            Excel, generated and distributed cost and labor reports
+            Excel, generated cost and labor reports
           </li>
-          <ul>
-            <li>
-              Co-published and presented to over 90 engineers an annual course
-              explaining the lifecycle of a Fixed Asset
-            </li>
-          </ul>
-          <ul>
-            <li>
-              Tracked CIP balance of $108 million and initiated the
-              capitalization/project closure process as needed
-            </li>
-          </ul>
           <li>
-            Took ownership of asset subledger by timely preparing
-            capitalizations, retirements, and cost adjustments as necessary
-            using SAP
+            Validated data and performed transactions as necessary using SAP
           </li>
         </ul>
 
@@ -141,25 +118,12 @@ class Experience extends Component {
         <i>Senior Financial Associate, June 2012- February 2014</i>
         <ul>
           <li>
-            Promoted to Senior Financial Associate in August 2013 from Financial
-            Associate{" "}
+            Managed and provided accounting support and reconciled accounts
+            during the month-end close
           </li>
           <li>
-            Managed and provided accounting support for the general ledger
-            during the month-end close for 7 plants
-          </li>
-          <li>
-            Created quarterly forecasts and managed the Field Operations
-            Incentive Plan for 17 plants using a detailed Excel spreadsheet
-          </li>
-          <li>
-            Performed extensive review of 170 account reconciliations, provided
-            feedback to reconciliation preparer, and presented findings to the
-            Balance Sheet Review team
-          </li>
-          <li>
-            Maintained the invoice/purchase order system and reconciled
-            approximately seventy accounts every month
+            Performed extensive review of account reconciliations, provided
+            feedback, and presented findings to upper management
           </li>
         </ul>
 
