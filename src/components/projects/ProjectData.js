c844da2f9e926db191bfaa6a4e9ea74305fe5d3f
Lizzie <lchan217@gmail.com>
2020-01-27 16:36:59 -0700

    update data

--- src/components/projects/ProjectData.js
+++ src/components/projects/ProjectData.js
@@ -2,7 +2,7 @@ const starwars = {
   image: "https://i.ibb.co/2gFX53Q/Screen-Shot-2020-01-27-at-4-13-31-PM.png",
   title: "Star Wars API",
   text:
-    "Web app that fetches from swapi.co and displays Star Wars movies and their characters upon click",
+    "Web app that fetches from swapi.co and displays Star Wars movies and their characters",
   pagelink: "https://star-wars-tech-challenge.netlify.com/",
   codelink: "https://github.com/lchan217/star-wars-api",
   footer: "Technologies and Frameworks: CSS and JavaScript"
@@ -12,7 +12,7 @@ const weather = {
   image: "https://i.ibb.co/xzcCdTR/Screen-Shot-2020-01-26-at-10-48-14-AM.png",
   title: "Weather App",
   text:
-    "Web app that fetches Munich weather data and displays daily average on cards and daily weather in 3 hour increments in a bar graph",
+    "Web app that fetches Munich weather data and displays daily temperature",
   pagelink: "https://weather-tech-challenge.netlify.com/",
   codelink: "https://github.com/lchan217/weather-app",
   footer:
@@ -21,8 +21,9 @@ const weather = {
 
 const singleColumn = {
   image: "https://i.ibb.co/DDMLxbh/Screen-Shot-2020-01-26-at-10-50-03-AM.png",
-  title: "Coming Soon Landing Page",
-  text: "Webpage that shows a landing page for a future company",
+  title: "Coming Soon Page",
+  text:
+    "Webpage that shows a landing page for a future company with email validation",
   pagelink: "https://coming-soon-page-example.netlify.com/",
   codelink: "https://github.com/kyoung90/Single-Column-Coming-Soon-Page",
   footer: "Technologies and Frameworks: CSS and JavaScript"
