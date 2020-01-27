7a34f93070749e593b3111d617c876b89edd359c
Lizzie <lchan217@gmail.com>
2020-01-27 16:27:08 -0700

    add intro paragraph

--- src/components/projects/MiniProjects.js
+++ src/components/projects/MiniProjects.js
@@ -5,6 +5,20 @@ import { Card } from "semantic-ui-react";
 
 const MiniProjectss = () => (
   <div>
+    <h1 className='center'>Mini Projects</h1>
+    <p>
+      This is a compilation of mini-projects from take-home interview tech
+      challenges and {""}
+      <a
+        href='https://www.frontendmentor.io/challenges'
+        rel='noopener noreferrer'
+        target='_blank'
+      >
+        frontend mentor challenges
+      </a>
+      . Any feedback is welcomed. Enjoy!
+    </p>
+    <br />
     <Card.Group>
       {ProjectData.map((project, index) => (
         <ProjectCard project={project} key={index} />
