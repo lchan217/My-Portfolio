4f9894624f3868c9d4b939a65ff2daa429836a33
Lizzie <lchan217@gmail.com>
2020-01-31 08:14:13 -0700

    add itemsperrow & change to class component

--- src/components/miniProjects/MiniProjects.js
+++ src/components/miniProjects/MiniProjects.js
@@ -1,33 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 import ProjectCard from "./MiniProjectCard";
 import { MiniProjectData } from "./MiniProjectData";
 import { Card } from "semantic-ui-react";
 
-let itemsPerRow;
-window.innerWidth > 500 ? (itemsPerRow = 3) : (itemsPerRow = 1);
+class MiniProjects extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      width: window.innerWidth
+    };
+  }
 
-const MiniProjects = () => (
-  <div>
-    <h1 className='center'>Mini Projects</h1>
-    <p>
-      This is a compilation of solo and collaboration mini-projects from
-      interview tech challenges and {""}
-      <a
-        href='https://www.frontendmentor.io/challenges'
-        rel='noopener noreferrer'
-        target='_blank'
-      >
-        frontend mentor challenges
-      </a>
-      . Any feedback is welcomed. Enjoy!
-    </p>
-    <br />
-    <Card.Group itemsPerRow={itemsPerRow}>
-      {MiniProjectData.map((project, index) => (
-        <ProjectCard project={project} key={index} />
-      ))}
-    </Card.Group>
-  </div>
-);
+  componentDidMount() {
+    window.addEventListener("resize", this.handleWindowSizeChange);
+  }
 
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
+
+  render() {
+    let itemsPerRow;
+    if (this.state.width >= 800) {
+      itemsPerRow = 3;
+    } else if (this.state.width < 800 && this.state.width >= 550) {
+      itemsPerRow = 2;
+    } else {
+      itemsPerRow = 1;
+    }
+
+    return (
+      <div>
+        <h1 className='center'>Mini Projects</h1>
+        <p>
+          This is a compilation of solo and collaboration mini-projects from
+          interview tech challenges and {""}
+          <a
+            href='https://www.frontendmentor.io/challenges'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            frontend mentor challenges
+          </a>
+          . Any feedback is welcomed. Enjoy!
+        </p>
+        <br />
+        <Card.Group itemsPerRow={itemsPerRow}>
+          {MiniProjectData.map((project, index) => (
+            <ProjectCard project={project} key={index} />
+          ))}
+        </Card.Group>
+      </div>
+    );
+  }
+}
 export default MiniProjects;
