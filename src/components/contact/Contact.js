1fec88c5db2fd43895af3d359efe6b7d2e01e428
Lizzie <lchan217@gmail.com>
2020-01-31 17:15:50 -0700

    change to stateless component and add semantic ui react image

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,58 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import ContactInfo from "./ContactInfo";
+import { Image } from "semantic-ui-react";
 
-class Contact extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      width: window.innerWidth
-    };
-  }
-
-  componentDidMount() {
-    window.addEventListener("resize", this.handleWindowSizeChange);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.handleWindowSizeChange);
-  }
-
-  handleWindowSizeChange = () => {
-    this.setState({ width: window.innerWidth });
-  };
-  render() {
-    let isMobile;
-    const width = this.state.width;
-    width <= 1000 ? (isMobile = true) : (isMobile = false);
-
-    let picHeight;
-    let picWidth;
-
-    if (isMobile) {
-      picWidth = 250;
-      picHeight = 200;
-    } else {
-      picWidth = 660;
-      picHeight = 500;
-    }
-
-    return (
-      <div className='contact-page center'>
-        <h1> Get In Touch </h1>
-        Feel free to reach out with any questions. I am always happy to connect!{" "}
-        <br /> <br />
-        <ContactInfo />
-        <br /> <br />
-        <img
-          width={picWidth}
-          height={picHeight}
-          src='https://i.ibb.co/nkydFFD/20180423-141220.jpg'
-          alt='pic of site owner'
-        />{" "}
-        <br />
-      </div>
-    );
-  }
-}
+const Contact = () => {
+  return (
+    <div className='contact-page center'>
+      <h1> Get In Touch </h1>
+      Feel free to reach out with any questions. I am always happy to connect!{" "}
+      <br /> <br />
+      <ContactInfo />
+      <br /> <br />
+      <Image
+        src='https://i.ibb.co/nkydFFD/20180423-141220.jpg'
+        alt='pic of site owner'
+        centered='true'
+      />
+      <br />
+    </div>
+  );
+};
 
 export default Contact;
